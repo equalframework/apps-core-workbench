8ae0e7f41ee35d9890add4c60f0d8cd5964408fc
anissaaab <60183@etu.he2b.be>
2025-03-07 09:33:53 +0100

    handle not duplication in explorer add

--- src/app/_dialogs/explorer-dialog/explorer-dialog.component.ts
+++ src/app/_dialogs/explorer-dialog/explorer-dialog.component.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, Component, Inject, OnInit, Optional, inject } 
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { ExplorerDialogFacade } from './explorer-dialog.facade';
 import { Observable } from 'rxjs';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 
 interface DialogData {
   fetchItems: (packageName: string) => Observable<any>;
@@ -20,84 +20,109 @@ export class ExplorerDialogComponent implements OnInit {
     fileForm: FormGroup;
 
 
-  packages$ = this.facade.packages$;
-  items$ = this.facade.items$;
-  selectedPackage$ = this.facade.selectedPackage$;
-  errorMessage$ = this.facade.errorMessage$
+    packages$ = this.facade.packages$;
+    items$ = this.facade.items$;
+    selectedPackage$ = this.facade.selectedPackage$;
+    errorMessage$ = this.facade.errorMessage$
 
-  // Added property for selected item
-  selectedItem: any = null;
-
-
-  constructor(
-    @Optional() public dialogRef: MatDialogRef<ExplorerDialogComponent>,
-    @Optional() @Inject(MAT_DIALOG_DATA) public data: DialogData,
-    private facade: ExplorerDialogFacade,
-    private fb: FormBuilder
-  ) {}
-
-  ngOnInit(): void {
-    this.createForm()
-  }
+    // Added property for selected item
+    selectedItem: any = null;
 
 
+    constructor(
+        @Optional() public dialogRef: MatDialogRef<ExplorerDialogComponent>,
+        @Optional() @Inject(MAT_DIALOG_DATA) public data: DialogData,
+        private facade: ExplorerDialogFacade,
+        private fb: FormBuilder
+    ) {}
 
-  onSelectPackage(packageName: string): void {
-    // Reset selected item when switching folders.
-    this.selectedItem = null;
-    this.fileForm.reset();
-    this.facade.selectPackage(packageName);
-    if (this.data?.fetchItems) {
-      this.facade.loadItems(this.data.fetchItems, packageName);
+    ngOnInit(): void {
+        this.createForm()
+        this.facade.items$.subscribe(items => {
+            const existingNames = items;
+            const control = this.fileForm.get('newItem');
+            if (control) {
+            control.setValidators([
+                Validators.required,
+                this.noDotsValidator,
+                this.duplicateValidator(existingNames)
+            ]);
+            control.updateValueAndValidity();
+            }
+        });
     }
-  }
 
-  // New method to handle item selection
-  onSelectItem(item: any): void {
-    this.selectedItem = item;
-  }
 
-  onCreateItem(): void {
-    if (this.fileForm.valid && this.data?.createItem) {
-      const newItem = this.fileForm.value.newItem;
 
-      const formattedItem = this.data.formatItem ? this.data.formatItem(newItem) : newItem;
-      const selectedPackage = this.facade.getSelectedPackage();
-      if (selectedPackage) {
-        // Use the optimistic method to add the item.
-        this.facade.optimisticCreateItem(this.data.createItem, formattedItem, selectedPackage);
-        // Reset the input field.
+    onSelectPackage(packageName: string): void {
+        // Reset selected item when switching folders.
+        this.selectedItem = null;
         this.fileForm.reset();
-      }
+        this.facade.selectPackage(packageName);
+        if (this.data?.fetchItems) {
+        this.facade.loadItems(this.data.fetchItems, packageName);
+        }
     }
-  }
 
-   getErrorMessage(controlName: string): string {
-    const control = this.fileForm.get(controlName);
-    if (control?.hasError('required')) {
-      return 'Item name is required.';
+    // New method to handle item selection
+    onSelectItem(item: any): void {
+        this.selectedItem = item;
     }
-    if (control?.hasError('invalidCharacter')) {
-      return 'The filename cannot contain a period (\'\').';
-    }
-    return '';
-  }
 
-  private createForm() {
-    this.fileForm = this.fb.group({
-      newItem: ['', [
-        Validators.required,
-        this.noDotsValidator,
-      ]]
-    });
-  }
+    onCreateItem(): void {
+        if (this.fileForm.valid && this.data?.createItem) {
+        const newItem = this.fileForm.value.newItem;
 
-  private noDotsValidator(control: any) {
-    if (control.value && control.value.includes('.')) {
-      return { invalidCharacter: true };
+        const formattedItem = this.data.formatItem ? this.data.formatItem(newItem) : newItem;
+        const selectedPackage = this.facade.getSelectedPackage();
+        if (selectedPackage) {
+            // Use the optimistic method to add the item.
+            this.facade.optimisticCreateItem(this.data.createItem, formattedItem, selectedPackage);
+            // Reset the input field.
+            this.fileForm.reset();
+        }
+        }
     }
-    return null;
-  }
+
+    getErrorMessage(controlName: string): string {
+        const control = this.fileForm.get(controlName);
+        if (control?.hasError('required')) {
+        return 'Item name is required.';
+        }
+        if (control?.hasError('invalidCharacter')) {
+        return 'The filename cannot contain a period (\'.\').';
+        }
+        if (control?.hasError('duplicate')) {
+        return 'Same name already exists';
+        }
+        return '';
+    }
+
+
+    private createForm() {
+        this.fileForm = this.fb.group({
+        newItem: ['', [
+            Validators.required,
+            this.noDotsValidator,
+        ]]
+        });
+    }
+
+    private noDotsValidator(control: any) {
+        if (control.value && control.value.includes('.')) {
+        return { invalidCharacter: true };
+        }
+        return null;
+    }
+
+    private duplicateValidator(existingNames: string[]) {
+        return (control: AbstractControl): ValidationErrors | null => {
+        if (control.value && existingNames.includes(this.data.formatItem ? this.data.formatItem(control.value) : control.value)) {
+            return { duplicate: true };
+        }
+        return null;
+        };
+    }
 
 
 
