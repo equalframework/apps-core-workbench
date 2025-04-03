e1e0487df292fd5a8f0754bcfea8daef76afde3c
anissaaab <60183@etu.he2b.be>
2025-04-03 09:08:07 +0200

    horizontal checkbox + adjust formatting (spaces)

--- src/app/_components/item-display/item-display.component.ts
+++ src/app/_components/item-display/item-display.component.ts
@@ -6,39 +6,39 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrls: ['./item-display.component.scss']
 })
 export class ItemDisplayComponent {
-  @Input() value: string = '';
-  @Input() icon: string = 'info';
-  @Input() tooltip: string = '';
-  @Input() editable: boolean = true;
-  @Input() deletable: boolean = true;
+    @Input() value: string = '';
+    @Input() icon: string = 'info';
+    @Input() tooltip: string = '';
+    @Input() editable: boolean = true;
+    @Input() deletable: boolean = true;
 
-  @Output() delete = new EventEmitter<void>();
-  @Output() itemClick = new EventEmitter<void>();
+    @Output() delete = new EventEmitter<void>();
+    @Output() itemClick = new EventEmitter<void>();
 
-  isEditing: boolean = false;
-  editedValue: string = '';
+    isEditing: boolean = false;
+    editedValue: string = '';
 
-  startEdit() {
-    if (!this.editable) return;
-    this.isEditing = true;
-    this.editedValue = this.value;
-  }
+    startEdit() {
+        if (!this.editable) return;
+        this.isEditing = true;
+        this.editedValue = this.value;
+    }
 
-  saveEdit() {
-    this.value = this.editedValue;
-    this.isEditing = false;
-  }
+    saveEdit() {
+        this.value = this.editedValue;
+        this.isEditing = false;
+    }
 
-  cancelEdit() {
-    this.isEditing = false;
-  }
+    cancelEdit() {
+        this.isEditing = false;
+    }
 
-  onDelete(event: Event) {
-    event.stopPropagation();
-    this.delete.emit();
-  }
+    onDelete(event: Event) {
+        event.stopPropagation();
+        this.delete.emit();
+    }
 
-  onClickItem() {
-  this.itemClick.emit();
-}
+    onClickItem() {
+    this.itemClick.emit();
+    }
 }
