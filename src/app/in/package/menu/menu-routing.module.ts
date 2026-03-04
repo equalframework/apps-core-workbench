4bb9255ebc503b36f2a2aa1e5591e8a9bb230f93
LarsHanquet <145200507+LarsHanquet@users.noreply.github.com>
2026-03-04 11:40:45 +0100

    Updated menu translations path

--- src/app/in/package/menu/menu-routing.module.ts
+++ src/app/in/package/menu/menu-routing.module.ts
@@ -1,19 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
 import { ModelTradEditorComponent } from '../model/model-trad-editor/model-trad-editor.component';
 import { PackageMenuComponent } from './package-menu.component';
 
 const routes: Routes = [
   // wildcard route (accept root and any sub route that does not match any of the routes above)
-/*
 
   {
       path: 'translations',
-      component: MenuEditorComponent
       component: ModelTradEditorComponent
   },
-*/
 
   {
       path: 'edit',
