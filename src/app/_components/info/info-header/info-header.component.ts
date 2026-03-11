ee40dea01ba4c3a3aba8697350457b39f3124b72
LarsHanquet <145200507+LarsHanquet@users.noreply.github.com>
2026-03-11 16:22:13 +0100

    Updated info-header component
    reorganized + handles 4 rows of data:
    - title + edit/translation buttons
    - description
    - metadata header
    - status

--- src/app/_components/info/info-header/info-header.component.ts
+++ src/app/_components/info/info-header/info-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter, AfterContentInit, ContentChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-info-header',
@@ -11,4 +11,24 @@ export class InfoHeaderComponent {
   @Input() copyText?: string;
   @Input() description!: string;
   @Input() metaData: { icon: string, tooltip: string, value: string, copyable?: boolean, double_backslash?:boolean }[] = [];
+
+  @Input() extraInfo: { label: string, value: string }[] = [];
+
+  @Input() statusInfo: { icon?: string, tooltip?: string, label: string, value: string }[] = [];
+
+  @Output() editClick = new EventEmitter<void>();
+  @Output() translationClick = new EventEmitter<void>();
+  @Output() extraClick = new EventEmitter<any>();
+
+  @ContentChild('[description]', { read: ElementRef }) projectedDescription: ElementRef | undefined;
+
+  public hasProjectedDescription: boolean = false;
+
+  get hasExtraInfo(): boolean {
+    return Array.isArray(this.extraInfo) && this.extraInfo.length > 0;
+  }
+
+  ngAfterContentInit(): void {
+    this.hasProjectedDescription = !!this.projectedDescription;
+  }
 }
