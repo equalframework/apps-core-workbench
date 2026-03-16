d59fdab3b6a31d6dfb3ccd3ca85abdf31c707482
LarsHanquet <145200507+LarsHanquet@users.noreply.github.com>
2026-03-16 13:12:12 +0100

    Updated views info to enable edition of translations

--- src/app/_components/info/info-view/info-view.component.ts
+++ src/app/_components/info/info-view/info-view.component.ts
@@ -97,11 +97,8 @@ export class InfoViewComponent implements OnInit, OnChanges, OnDestroy {
     }
 
     public onclickTranslations() {
-        // #todo - depends on route assigned to translation
-        console.log('Not implemented yet');
-        /*
-        this.router.navigate([`/package/${this.view.package_name}/entity/${this.view.item.model}/view/${this.view.name.split(".")[1]}/type/${this.view.name.split(":")[1].split(".")[0]}/translations`]);
-        */
+        this.router.navigate([`/package/${this.view.package_name}/model/${this.view.item.model}/translations`], 
+        { queryParams: { 'tab': 'view', 'view': this.view.name } });
     }
 
 }
