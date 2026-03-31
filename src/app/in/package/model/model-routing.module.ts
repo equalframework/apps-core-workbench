c946485de694ec03c6f50dbd8a511d07b269e956
LarsHanquet <145200507+LarsHanquet@users.noreply.github.com>
2026-03-31 12:10:07 +0200

    Removed duplicate routing in model

--- src/app/in/package/model/model-routing.module.ts
+++ src/app/in/package/model/model-routing.module.ts
@@ -5,39 +5,6 @@ import { PackageModelComponent } from './package-model.component';
 import { ModelTradEditorModule } from './model-trad-editor/model-trad-editor.module';
 
 const routes: Routes = [
-    // wildcard route (accept root and any sub route that does not match any of the routes above)
-    {
-        path: 'fields',
-        loadChildren: () => import('./fields/fields.module').then(m => m.PackageModelFieldsModule)
-    },
-    {
-        path: 'views',
-        loadChildren: () => import('./views/views.module').then(m => m.PackageModelViewsModule)
-    },
-    {
-        path: 'translations',
-        loadChildren: () => import('./model-trad-editor/model-trad-editor.module').then(m=>ModelTradEditorModule)
-    },
-    {
-        path: 'workflow',
-        loadChildren: () => import('./workflow/workflow.module').then(m => m.PackageModelWorkflowModule)
-    },
-    {
-        path: 'policies',
-        loadChildren: () => import('./policies/policies.module').then(m => m.PackageModelPoliciesModule)
-    },
-    {
-        path: 'actions',
-        loadChildren: () => import('./actions/actions.module').then(m => m.PackageModelActionsModule)
-    },
-    {
-        path:'roles',
-        loadChildren: () => import('./roles/roles.module').then(m => m.PackageModelRolesModule)
-    },
-    {
-        path: 'model/:name',
-        component: PackageModelComponent
-    },
     {
         path: '',
         component: PackageModelComponent
