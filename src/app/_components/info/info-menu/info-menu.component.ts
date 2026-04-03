3442338ef99fb76d4c0c85d648bb8b794540f494
LarsHanquet <145200507+LarsHanquet@users.noreply.github.com>
2026-04-03 12:10:50 +0200

    Updated info-menu to allow JSONSchema status display

--- src/app/_components/info/info-menu/info-menu.component.ts
+++ src/app/_components/info/info-menu/info-menu.component.ts
@@ -1,28 +1,86 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, SimpleChanges, OnDestroy, OnChanges } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 import { RouterMemory } from 'src/app/_services/routermemory.service';
 import { EqualComponentDescriptor } from 'src/app/in/_models/equal-component-descriptor.class';
+import { WorkbenchService } from 'src/app/in/_services/workbench.service';
+import { JsonValidationService, ValidationStatusInfo } from 'src/app/in/_services/json-validation.service';
 
 @Component({
   selector: 'info-menu',
   templateUrl: './info-menu.component.html',
   styleUrls: ['./info-menu.component.scss']
 })
-export class InfoMenuComponent implements OnInit {
+export class InfoMenuComponent implements OnInit, OnChanges, OnDestroy {
 
     @Input() menu: EqualComponentDescriptor;
     @Input() selected_package: string;
 
+    public loading: boolean = true;
+    public menuSchema: any = null;
+    public validationStatus: ValidationStatusInfo[] = [];
+    private destroy$ = new Subject<void>();
+
     constructor(
-            private router: RouterMemory
+            private router: RouterMemory,
+            private workbenchService: WorkbenchService,
+            private jsonValidationService: JsonValidationService
         ) { }
 
     public ngOnInit(): void {
-        this.selected_package = this.menu.package_name;
+        this.loadMenu();
     }
 
-    public ngOnChanges(): void {
+    public ngOnChanges(changes: SimpleChanges): void {
+        if (changes.menu) {
+            this.loadMenu();
+        }
+    }
+
+    public ngOnDestroy(): void {
+        this.destroy$.next();
+        this.destroy$.complete();
+    }
+
+    private loadMenu(): void {
+        if (!this.menu?.package_name || !this.menu?.name) {
+            this.loading = false;
+            this.validationStatus = this.jsonValidationService.buildStatusInfo('JSON schema', null, false, 'No menu selected');
+            return;
+        }
+
         this.selected_package = this.menu.package_name;
-        console.log('Menu changed: ', this.selected_package);
+        this.loading = true;
+        this.validationStatus = this.jsonValidationService.buildStatusInfo('JSON schema', null, true);
+
+        this.workbenchService.readMenu(this.menu.package_name, this.menu.name)
+            .pipe(takeUntil(this.destroy$))
+            .subscribe({
+                next: (data) => {
+                    this.menuSchema = data;
+                    this.validateSchema();
+                    this.loading = false;
+                },
+                error: () => {
+                    this.loading = false;
+                    this.validationStatus = this.jsonValidationService.buildStatusInfo('JSON schema', null, false, 'Unable to load menu schema');
+                }
+            });
+    }
+
+    private validateSchema(): void {
+        this.jsonValidationService.validate(
+            this.menuSchema,
+            'urn:equal:json-schema:core:menu',
+            this.menu?.package_name
+        ).subscribe({
+            next: (result) => {
+                this.validationStatus = this.jsonValidationService.buildStatusInfo('JSON schema', result);
+            },
+            error: () => {
+                this.validationStatus = this.jsonValidationService.buildStatusInfo('JSON schema', null, false, 'Unable to validate menu');
+            }
+        });
     }
 
     public onclickTranslations() {
