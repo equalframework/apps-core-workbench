090707073e0cf80913159b18137ebbc3a1b5d4d3
LarsHanquet <145200507+LarsHanquet@users.noreply.github.com>
2026-04-10 12:25:21 +0200

    Linted toggling-button

--- src/app/in/package/view/_components/toggling-button/toggling-button.component.spec.ts
+++ src/app/in/package/view/_components/toggling-button/toggling-button.component.spec.ts
@@ -34,4 +34,24 @@ describe('TogglingButtonComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should toggle baseState from true to false', () => {
+    component.baseState = true;
+    component.toggleIt();
+    expect(component.baseState).toBe(false);
+  });
+
+  it('should toggle baseState from false to true', () => {
+    component.baseState = false;
+    component.toggleIt();
+    expect(component.baseState).toBe(true);
+  });
+
+  it('should emit new state on toggleIt', () => {
+    component.baseState = true;
+    let emittedValue: boolean | undefined;
+    component.toggle.subscribe((val: boolean) => emittedValue = val);
+    component.toggleIt();
+    expect(emittedValue).toBe(false);
+  });
 });
