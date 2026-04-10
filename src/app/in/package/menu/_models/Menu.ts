bcfdd7b0a59396b5c9d6dc3ea56adfb6a051204c
LarsHanquet <145200507+LarsHanquet@users.noreply.github.com>
2026-04-10 12:34:15 +0200

    Linted menu models

--- src/app/in/package/menu/_models/Menu.ts
+++ src/app/in/package/menu/_models/Menu.ts
@@ -1,74 +1,74 @@
 
 export class MenuElement {
-    public leftover:any = {};
+    public leftover: any = {};
 
     constructor() {}
 
-    public export() {
-        let result:any = {};
-        for(let key in this.leftover) {
-            if(this.leftover[key]) result[key] = this.leftover[key];
+    public export(): any {
+        const result: any = {};
+        for (const key in this.leftover) {
+            if (this.leftover[key]) { result[key] = this.leftover[key]; }
         }
         return result;
     }
 
-    public id_compliancy(id_list:string[]):{ok:boolean,id_list:string[]} {
-        return {ok : true, id_list : id_list};
+    public idCompliancy(idList: string[]): {ok: boolean, idList: string[]} {
+        return {ok : true, idList : idList};
     }
 }
 
 export class Menu extends MenuElement {
-    public name:string = '';
-    public access:any = {'groups': ['users']};
-    public layout:MenuLayout = new MenuLayout();
+    public name = '';
+    public access: any = {'groups': ['users']};
+    public layout: MenuLayout = new MenuLayout();
 
-    constructor(schema:any = {}) {
+    constructor(schema: any = {}) {
         super();
 
-        if(schema.name) {
+        if (schema.name) {
             this.name = schema.name;
             delete schema.name;
         }
-        if(schema.access) {
+        if (schema.access) {
             this.access = schema.access;
             delete schema.access;
         }
-        if(schema.layout) {
+        if (schema.layout) {
             this.layout = new MenuLayout(schema.layout);
             delete schema.layout;
         }
         this.leftover = schema;
     }
 
-    override export() {
-        let result = super.export();
+    override export(): any {
+        const result = super.export();
 
-        if(this.name) {
+        if (this.name) {
             result.name = this.name;
         }
-        if(this.access) {
+        if (this.access) {
             result.access = this.access;
         }
-        if(this.layout){
+        if (this.layout){
             result.layout = this.layout.export();
         }
         return result;
     }
 
-    override id_compliancy(id_list: string[]): { ok: boolean; id_list: string[]; } {
-        let res = super.id_compliancy([]);
-        return this.layout.id_compliancy(res.id_list);
+    override idCompliancy(idList: string[]): { ok: boolean; idList: string[]; } {
+        const res = super.idCompliancy([]);
+        return this.layout.idCompliancy(res.idList);
     }
 }
 
 export class MenuLayout extends MenuElement {
-    public items:MenuItem[] = [];
+    public items: MenuItem[] = [];
 
-    constructor(schema:any = {}) {
+    constructor(schema: any = {}) {
         super();
 
-        if(schema.items) {
-            for(let item of schema.items) {
+        if (schema.items) {
+            for (const item of schema.items) {
                 this.items.push(new MenuItem(item));
             }
             delete schema.item;
@@ -77,121 +77,129 @@ export class MenuLayout extends MenuElement {
         this.leftover = schema.item;
     }
 
-    override export() {
-        let result = super.export();
+    override export(): any {
+        const result = super.export();
 
-        if(this.items.length > 0) {
+        if (this.items.length > 0) {
             result.items = [];
-            for(let item of this.items) {
+            for (const item of this.items) {
                 result.items.push(item.export());
             }
         }
-        return result
+        return result;
     }
 
-    override id_compliancy(id_list: string[]): { ok: boolean; id_list: string[]; } {
-        let res = super.id_compliancy([]);
+    override idCompliancy(idList: string[]): { ok: boolean; idList: string[]; } {
+        let res = super.idCompliancy([]);
 
-        for(let item of this.items) {
-            res = item.id_compliancy(res.id_list);
-            if(!res.ok) {
+        for (const item of this.items) {
+            res = item.idCompliancy(res.idList);
+            if (!res.ok) {
                 return res;
             }
         }
-        return res
+        return res;
     }
 }
 
 export class MenuItem extends MenuElement {
-    public id:string = '';
-    public label:string = '';
-    public icon:string = ''
-    public type:string = 'entry';
-    public route:string = '';
+    public id = '';
+    public label = '';
+    public icon = '';
+    public type = 'entry';
+    public route = '';
 
-    public description:string = '';
-    children:MenuItem[] = [];
-    context:MenuContext = new MenuContext();
+    public description = '';
+    children: MenuItem[] = [];
+    context: MenuContext = new MenuContext();
 
-    public static incrementer:number = 0;
-    public static get availableTypes() { return ['parent','entry'] }
+    public static menuItemCount = 0;
+    public static get availableTypes(): string[] {
+        return ['parent', 'entry'];
+    }
 
-    constructor(schema:any = {}) {
+    constructor(schema: any = {}) {
         super();
 
-        if(schema.id) {
-            this.id = schema.id;delete schema.id;
+        if (schema.id) {
+            this.id = schema.id;
+            delete schema.id;
         }
-        if(schema.label) {
-            this.label = schema.label;delete schema.label;
+        if (schema.label) {
+            this.label = schema.label;
+            delete schema.label;
         }
-        if(schema.icon) {
-            this.icon = schema.icon;delete schema.icon;
+        if (schema.icon) {
+            this.icon = schema.icon;
+            delete schema.icon;
         }
-        if(schema.type) {
-            this.type = schema.type;delete schema.type;
+        if (schema.type) {
+            this.type = schema.type;
+            delete schema.type;
         }
-        if(schema.description) {
-            this.description = schema.description;delete schema.description;
+        if (schema.description) {
+            this.description = schema.description;
+            delete schema.description;
         }
-        if(schema.route) {
-            this.route = schema.route;delete schema.route;
+        if (schema.route) {
+            this.route = schema.route;
+            delete schema.route;
         }
 
-        if(schema.children) {
-            for(let child of schema.children) {
+        if (schema.children) {
+            for (const child of schema.children) {
                 this.children.push(new MenuItem(child));
             }
             delete schema.children;
         }
-        if(schema.context) {
+        if (schema.context) {
             this.context = new MenuContext(schema.context);
             delete schema.context;
         }
         this.leftover = schema;
     }
 
-    override export() {
-        let result = super.export()
+    override export(): any {
+        const result = super.export();
 
-        if(this.id) {
+        if (this.id) {
             result.id = this.id;
         }
-        if(this.type) {
+        if (this.type) {
             result.type = this.type;
         }
-        if(this.label) {
+        if (this.label) {
             result.label = this.label;
         }
-        if(this.icon) {
+        if (this.icon) {
             result.icon = this.icon;
         }
-        if(this.description) {
+        if (this.description) {
             result.description = this.description;
         }
-        if(this.route) {
+        if (this.route) {
             result.route = this.route;
         }
-        if(this.context) {
+        if (this.context) {
             result.context = this.context.export();
         }
-        if(this.children) {
+        if (this.children) {
             result.children = this.children.map(child => child.export());
         }
         return result;
     }
 
 
-    override id_compliancy(id_list: string[]): { ok: boolean; id_list: string[]; } {
-        let res = super.id_compliancy(id_list);
-        if(res.id_list.includes(this.id)) {
-            return {ok : false, id_list : [...res.id_list,this.id]};
+    override idCompliancy(idList: string[]): { ok: boolean; idList: string[]; } {
+        let res = super.idCompliancy(idList);
+        if (res.idList.includes(this.id)) {
+            return {ok : false, idList : [...res.idList, this.id]};
         }
         else {
-            res.id_list.push(this.id)
-            for(let item of this.children) {
-                res = item.id_compliancy(res.id_list);
-                if(!res.ok) {
+            res.idList.push(this.id);
+            for (const item of this.children) {
+                res = item.idCompliancy(res.idList);
+                if (!res.ok) {
                     return res;
                 }
             }
@@ -201,15 +209,15 @@ export class MenuItem extends MenuElement {
 }
 
 export class MenuContext extends MenuElement {
-    public entity:string = '';
-    public view:string = '';
-    public domain:any = [];
-    public sort:'asc'|'desc' = 'asc';
-    public order:string = '';
-    public purpose:string = '';
-    public display_mode:string = '';
+    public entity = '';
+    public view = '';
+    public domain: any = [];
+    public sort: 'asc'|'desc' = 'asc';
+    public order = '';
+    public purpose = '';
+    public displayMode = '';
 
-    static get PossiblePurpose():string[] {
+    static get PossiblePurpose(): string[] {
         return [
             '',
             'view',
@@ -220,7 +228,7 @@ export class MenuContext extends MenuElement {
         ];
     }
 
-    static get PossibleDisplayMode():string[] {
+    static get PossibleDisplayMode(): string[] {
         return [
             '',
             'stacked',
@@ -228,69 +236,69 @@ export class MenuContext extends MenuElement {
         ];
     }
 
-    public _has_domain:boolean = false;
-    public _has_sort:boolean = false;
-    public _has_order:boolean = false;
+    public _hasDomain = false;
+    public _hasSort = false;
+    public _hasOrder = false;
 
-    constructor(schema:any = {}) {
+    constructor(schema: any = {}) {
         super();
 
-        if(schema.entity) {
+        if (schema.entity) {
             this.entity = schema.entity;
             delete schema.description;
         }
-        if(schema.view) {
+        if (schema.view) {
             this.view = schema.view;
             delete schema.description;
         }
-        if(schema.domain) {
-            this._has_domain = true;
+        if (schema.domain) {
+            this._hasDomain = true;
             this.domain = schema.domain;
             delete schema.domain;
         }
-        if(schema.sort) {
-            this._has_sort = true;
+        if (schema.sort) {
+            this._hasSort = true;
             this.sort = schema.sort;
             delete schema.sort;
         }
-        if(schema.order) {
-            this._has_order = true;
+        if (schema.order) {
+            this._hasOrder = true;
             this.order = schema.order;
             delete schema.order;
         }
-        if(schema.purpose) {
+        if (schema.purpose) {
             this.purpose = schema.purpose;
             delete schema.purpose;
         }
-        if(schema.display_mode) {
-            this.display_mode = schema.display_mode;
+        if (schema.display_mode) {
+            this.displayMode = schema.display_mode;
             delete schema.display_mode;
         }
     }
 
-    override export() {
-        let result = super.export();
+    override export(): any {
+        const result = super.export();
 
-        if(this.entity) {
+        if (this.entity) {
             result.entity = this.entity;
         }
-        if(this.view) {
+        if (this.view) {
             result.view = this.view;
         }
-        if(this.domain && this._has_domain) {
+        if (this.domain && this._hasDomain) {
             result.domain = this.domain;
         }
-        if(this.sort && this._has_sort) {
+        if (this.sort && this._hasSort) {
             result.sort = this.sort;
         }
-        if(this.order && this._has_order) {
+        if (this.order && this._hasOrder) {
             result.order = this.order;
         }
-        if(this.purpose) {
+        if (this.purpose) {
             result.purpose = this.purpose;
         }
-        if(this.display_mode) {
-            result.display_mode = this.display_mode;
+        if (this.displayMode) {
+            result.display_mode = this.displayMode;
         }
 
         return result;
