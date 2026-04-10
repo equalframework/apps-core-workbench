a360d707ed67387f29ab0719c4712a54cf2ff99a
LarsHanquet <145200507+LarsHanquet@users.noreply.github.com>
2026-04-10 12:31:52 +0200

    Linted model classes

--- src/app/in/package/model/_object/FieldClass.ts
+++ src/app/in/package/model/_object/FieldClass.ts
@@ -1,68 +1,69 @@
-import { cloneDeep } from "lodash";
+import { cloneDeep } from 'lodash';
 
 export class FieldClass {
-    public name:string;
-    public sync_name:string;
-    public synchronised:boolean;
-    public inherited:boolean
-    public current_scheme:any
-    public sync_scheme:any = undefined
-    public isNew:boolean = false
-    public deleted:boolean = false
+    public name: string;
+    public syncName: string;
+    public isSynchronized: boolean;
+    public inherited: boolean;
+    public currentSchema: any;
+    public syncSchema: any = undefined;
+    public isNew = false;
+    public deleted = false;
 
     // Default value for field
-    static get defaultModel(){
-        return {"type":"string"}
+    static get defaultModel(): any {
+        return {type: 'string'};
     }
 
-    constructor(name:string,inherited:boolean=false,synchronised:boolean=true,scheme:any=FieldClass.defaultModel) {
+    constructor(name: string, inherited: boolean= false, isSynchronized: boolean= true, scheme: any= FieldClass.defaultModel) {
         this.name = name;
-        this.sync_name = this.name
+        this.syncName = this.name;
         this.inherited = inherited;
-        this.synchronised = synchronised;
-        this.isNew = !synchronised;
-        this.sync_scheme = cloneDeep(scheme);
-        this.current_scheme = cloneDeep(scheme);
+        this.isSynchronized = isSynchronized;
+        this.isNew = !isSynchronized;
+        this.syncSchema = cloneDeep(scheme);
+        this.currentSchema = cloneDeep(scheme);
     }
 
-    public checkSync():boolean {
+    public checkSync(): boolean {
         // TODO : Make this work
-        this.synchronised = compareDictRecursif(this.sync_scheme,this.current_scheme) === 0 && this.sync_name === this.name && !this.deleted
-        return this.synchronised
+        this.isSynchronized = compareDictRecurs(this.syncSchema, this.currentSchema) === 0 && this.syncName === this.name && !this.deleted;
+        return this.isSynchronized;
     }
 }
 
 /**
  * @description
- * Compare dict1 and dict2 recusrively (could have better optimisation)
- * @param dict1 
- * @param dict2 
+ * Compare dict1 and dict2 recursively (could have better optimization)
+ * @param dict1 Either dict1 or dict2 can be undefined, if one of them is undefined and the other is not, the result is not equal, if both of them are undefined, the result is equal
+ * If dict1 and dict2 are not dict, they are compared with === operator
+ * @param dict2 Second dict to compare with dict1
  * @returns dict1 === dict2
  */
-function compareDictRecursif(dict1:any, dict2:any):number {
+function compareDictRecurs(dict1: any, dict2: any): number {
     try {
-        if(dict1 === undefined) return -1
-        if(dict2 === undefined) return 1
-        if(typeof(dict1) !== typeof({}) && typeof(dict1) !== typeof({}) && dict1 === dict2) {
-            return 0
+        if (dict1 === undefined) { return -1; }
+        if (dict2 === undefined) { return 1; }
+        if (typeof(dict1) !== typeof({}) && typeof(dict1) !== typeof({}) && dict1 === dict2) {
+            return 0;
         }
-        if(typeof(dict1) !== typeof({}) && typeof(dict1) !== typeof({}) && dict1 !== dict2) {
-            return -1
+        if (typeof(dict1) !== typeof({}) && typeof(dict1) !== typeof({}) && dict1 !== dict2) {
+            return -1;
         }
-        var res:number
-        for(var item in dict1) {
-            if(dict2[item] === undefined) return 1
-            res = compareDictRecursif(dict1[item],dict2[item])
-            if(res !== 0) return 1
+        let res: number;
+        for (const item in dict1) {
+            if (dict2[item] === undefined) { return 1; }
+            res = compareDictRecurs(dict1[item], dict2[item]);
+            if (res !== 0) { return 1; }
         }
-        for(var item in dict2) {
-            if(dict1[item] === undefined) return 1
-            res = compareDictRecursif(dict1[item],dict2[item])
-            if(res !== 0) return -1
+        for (const item in dict2) {
+            if (dict1[item] === undefined) { return 1; }
+            res = compareDictRecurs(dict1[item], dict2[item]);
+            if (res !== 0) { return -1; }
         }
     } catch {
-        return 0
+        return 0;
     }
-    return 0
+    return 0;
 }
 
--- src/app/in/package/model/_object/FieldClassArray.ts
+++ src/app/in/package/model/_object/FieldClassArray.ts
@@ -1,44 +1,44 @@
-import { FieldClass } from "./FieldClass";
+import { FieldClass } from './FieldClass';
 
 export class FieldClassArray extends Array<FieldClass> {
-    public getNameList():string[] {
-        var result:string[] = [];
-        for(var field in this) {
-            result.push(this[field].name)
+    public getNameList(): string[] {
+        let result: string[] = [];
+        for (const field in this) {
+            result.push(this[field].name);
         }
         return result;
     }
-    public getInheritDict():{[id:string]:boolean} {
-        var result:{[id:string]:boolean} = {}
-        for(var field in this) {
-            result[this[field].name] = this[field].inherited
+    public getInheritDict(): {[id: string]: boolean} {
+        let result: {[id: string]: boolean} = {};
+        for (const field in this) {
+            result[this[field].name] = this[field].inherited;
         }
-        return result
+        return result;
     }
-    public getByName(name:string):FieldClass|undefined {
-        for(var field in this) {
-            if(this[field].name === name) {
-                return this[field]
+    public getByName(name: string): FieldClass|undefined {
+        for (const field in this) {
+            if (this[field].name === name) {
+                return this[field];
             }
         }
-        return undefined
+        return undefined;
     }
-    public removeByName(name:string) {
-        for(var  i = 0 ; i < this.length ; i++) {
-            if(this[i].name == name) {
-                this.splice(i,1)
+    public removeByName(name: string): void {
+        for (let  i = 0 ; i < this.length ; i++) {
+            if (this[i].name === name) {
+                this.splice(i, 1);
             }
         }
     }
-    public override push(...item:FieldClass[]):number{
-        var cpt:number = 0
-        for(var i=0; i< item.length ; i++) {
-            if(this.getByName(item[i].name) !== undefined) {
-                continue
+    public override push(...item: FieldClass[]): number{
+        let cpt = 0;
+        for (const field in item) {
+             if (this.getByName(item[field].name) !== undefined) {
+                continue;
             }
-            super.push(item[i])
-            cpt ++
+            super.push(item[field]);
+            cpt ++;
         }
-        return cpt
+        return cpt;
     }
 }
