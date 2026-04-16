942a3d4de04c33f099a424c62c0103a6feeae9f3
LarsHanquet <145200507+LarsHanquet@users.noreply.github.com>
2026-04-16 16:40:42 +0200

    Fixed tests imports for pipeline components properties-editor

--- src/app/in/pipeline/_components/properties-editor/properties-editor.component.spec.ts
+++ src/app/in/pipeline/_components/properties-editor/properties-editor.component.spec.ts
@@ -1,4 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { OverlayModule } from '@angular/cdk/overlay';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TranslateModule } from '@ngx-translate/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 
 import { PropertiesEditorComponent } from './properties-editor.component';
 
@@ -8,7 +14,12 @@ describe('PropertiesEditorComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PropertiesEditorComponent ]
+      declarations: [ PropertiesEditorComponent ],
+      imports: [ HttpClientTestingModule, OverlayModule, TranslateModule.forRoot() ],
+      providers: [ MatSnackBar,
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+        { provide: MatDialog, useValue: {} }
+       ]
     })
     .compileComponents();
   });
