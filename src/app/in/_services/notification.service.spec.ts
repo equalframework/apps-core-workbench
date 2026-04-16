5a072e15436518a1c28e43bddf90896b4f26cf07
LarsHanquet <145200507+LarsHanquet@users.noreply.github.com>
2026-04-16 16:54:27 +0200

    Added tests for notification service

--- src/app/in/_services/notification.service.spec.ts
+++ src/app/in/_services/notification.service.spec.ts
@@ -1,16 +1,40 @@
 import { TestBed } from '@angular/core/testing';
-
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { OverlayModule } from '@angular/cdk/overlay';
 import { NotificationService } from './notification.service';
 
 describe('NotificationService', () => {
   let service: NotificationService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [OverlayModule],
+      providers: [MatSnackBar]
+    });
     service = TestBed.inject(NotificationService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should show success notification', () => {
+    spyOn(service, 'showSuccess');
+    service.showSuccess('Success message');
+    expect(service.showSuccess).toHaveBeenCalledWith('Success message');
+  });
+
+  it('should show error notification', () => {
+    spyOn(service, 'showError');
+    service.showError('Error message');
+    expect(service.showError).toHaveBeenCalledWith('Error message');
+  });
+
+  it('should show info notification', () => {
+    spyOn(service, 'showInfo');
+    service.showInfo('Info message', 'Close', 3000);
+    expect(service.showInfo).toHaveBeenCalledWith('Info message', 'Close', 3000);
+  });
+
+  
 });
