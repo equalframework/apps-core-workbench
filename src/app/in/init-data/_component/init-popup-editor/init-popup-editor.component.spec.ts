cf262594ca8862e49437b04842429ef63f92844f
LarsHanquet <145200507+LarsHanquet@users.noreply.github.com>
2026-04-16 16:52:44 +0200

    Fixed tests components in init-data

--- src/app/in/init-data/_component/init-popup-editor/init-popup-editor.component.spec.ts
+++ src/app/in/init-data/_component/init-popup-editor/init-popup-editor.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { InitPopupEditorComponent } from './init-popup-editor.component';
+import { InitDataEntityInstance } from '../../_models/init-data';
 
 describe('InitPopupEditorComponent', () => {
   let component: InitPopupEditorComponent;
@@ -8,7 +9,11 @@ describe('InitPopupEditorComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ InitPopupEditorComponent ]
+      declarations: [ InitPopupEditorComponent ],
+      providers: [
+        { provide: MatDialogRef, useValue: jasmine.createSpyObj('MatDialogRef', ['close']) },
+        { provide: MAT_DIALOG_DATA, useValue: { fields: [], data: new InitDataEntityInstance(1, []) } }
+      ]
     })
     .compileComponents();
   });
--- src/app/in/init-data/_component/init-sidepane/init-sidepane.component.spec.ts
+++ src/app/in/init-data/_component/init-sidepane/init-sidepane.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TranslateModule } from '@ngx-translate/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { InitSidepaneComponent } from './init-sidepane.component';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { OverlayModule } from '@angular/cdk/overlay';
 
 describe('InitSidepaneComponent', () => {
   let component: InitSidepaneComponent;
@@ -8,7 +13,15 @@ describe('InitSidepaneComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ InitSidepaneComponent ]
+      declarations: [ InitSidepaneComponent ],
+      imports: [ TranslateModule.forRoot(), HttpClientTestingModule ],
+      providers: [
+        { provide: MatDialog, useValue: {} },
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+        { provide: MatSnackBar, useValue: {} },
+        { provide: OverlayModule, useValue: {} }
+      ]
+
     })
     .compileComponents();
   });
