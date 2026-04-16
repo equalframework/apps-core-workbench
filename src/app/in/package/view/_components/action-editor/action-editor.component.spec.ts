f9a0c68cf7dd28665c75a54fd364b64e287551e0
LarsHanquet <145200507+LarsHanquet@users.noreply.github.com>
2026-04-16 16:43:19 +0200

    Added tests for view action editor components

--- src/app/in/package/view/_components/action-editor/action-editor.component.spec.ts
+++ src/app/in/package/view/_components/action-editor/action-editor.component.spec.ts
@@ -1,32 +1,79 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ActionEditorComponent } from './action-editor.component';
-import { ViewAction, ViewHeader } from '../../_objects/View';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatAutocomplete } from '@angular/material/autocomplete';
+import { ViewAction } from '../../_objects/View';
+import { MatDialog } from '@angular/material/dialog';
+import { PopupParamsComponent } from './_components/popup-params/popup-params.component';
 
 describe('ActionEditorComponent', () => {
   let component: ActionEditorComponent;
   let fixture: ComponentFixture<ActionEditorComponent>;
+  let matDialogSpy: jasmine.SpyObj<MatDialog>;
 
   beforeEach(async () => {
+    matDialogSpy = jasmine.createSpyObj('MatDialog', ['open']);
+
     await TestBed.configureTestingModule({
-      declarations: [ ActionEditorComponent, MatAutocomplete ],
-      imports: [MatDialogModule]
+      declarations: [ActionEditorComponent],
+      providers: [{ provide: MatDialog, useValue: matDialogSpy }]
     })
+    .overrideTemplate(ActionEditorComponent, '')
     .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ActionEditorComponent);
     component = fixture.componentInstance;
-    component.controllers = [];
+    component.controllers = ['processOrder', 'cancelOrder', 'auditTrail'];
     component.obj = new ViewAction();
     fixture.detectChanges();
-    component.obj = new ViewAction();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize filtered options with empty and controller values', () => {
+    component.ngOnInit();
+
+    expect(component.filteredOptions).toEqual(['', 'processOrder', 'cancelOrder', 'auditTrail']);
+  });
+
+  it('should emit delete event when deleteMe is called', () => {
+    spyOn(component.delete, 'emit');
+
+    component.deleteMe();
+
+    expect(component.delete.emit).toHaveBeenCalled();
+  });
+
+  it('should filter options based on controller value on tap', () => {
+    component.obj.controller = 'order';
+
+    component.tap('ignored-value');
+
+    expect(component.filteredOptions).toEqual(['', 'processOrder', 'cancelOrder']);
+  });
+
+  it('should open custom params popup with current action data', () => {
+    component.show_custom_params();
+
+    expect(matDialogSpy.open).toHaveBeenCalledWith(PopupParamsComponent, { data: component.obj });
+  });
+
+  it('should set bigDisp to true when changeBigDispBy is called with true', () => {
+    component.bigDisp = false;
+
+    component.changeBigDispBy(true);
+
+    expect(component.bigDisp).toBeTrue();
+  });
+
+  it('should not set bigDisp to false when changeBigDispBy is called with false', () => {
+    component.bigDisp = true;
+
+    component.changeBigDispBy(false);
+
+    expect(component.bigDisp).toBeTrue();
   });
 });
--- src/app/in/package/view/_components/actions-container/actions-container.component.spec.ts
+++ src/app/in/package/view/_components/actions-container/actions-container.component.spec.ts
@@ -1,8 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { ActionsContainerComponent } from './actions-container.component';
 import { ViewAction } from '../../_objects/View';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
+
+@Component({
+  selector: 'app-action-editor',
+  template: ''
+})
+class ActionEditorStubComponent {
+  @Input() obj!: ViewAction;
+  @Input() controllers!: string[];
+  @Input() groups!: string[];
+  @Input() packageName!: string;
+  @Input() entity!: string;
+  @Input() actionIndex!: number;
+  @Output() delete = new EventEmitter<void>();
+}
 
 describe('ActionsContainerComponent', () => {
   let component: ActionsContainerComponent;
@@ -10,7 +27,8 @@ describe('ActionsContainerComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ActionsContainerComponent ]
+      declarations: [ ActionsContainerComponent, ActionEditorStubComponent ],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
   });
@@ -37,6 +55,23 @@ describe('ActionsContainerComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  it('should show info label when there are no actions', () => {
+    component.acts = [];
+    fixture.detectChanges();
+
+    const info = fixture.debugElement.query(By.css('.info'));
+    expect(info).toBeTruthy();
+    expect(info.nativeElement.textContent).toContain('No custom actions bound.');
+  });
+
+  it('should hide info label when actions exist', () => {
+    component.acts = [new ViewAction()];
+    fixture.detectChanges();
+
+    const info = fixture.debugElement.query(By.css('.info'));
+    expect(info).toBeFalsy();
+  });
+
   it('should delete action from list', () => {
     const action = new ViewAction();
     component.acts = [action];
@@ -55,5 +90,71 @@ describe('ActionsContainerComponent', () => {
     component.domainLabels = false;
     component.create();
     expect(component.acts.length).toBe(1);
+  });
+
+  it('should create a domainable action when domain labels are enabled', () => {
+    component.acts = [];
+    component.domainLabels = true;
+
+    component.create();
+
+    expect(component.acts.length).toBe(1);
+    expect(component.acts[0]._domainable).toBeTrue();
+  });
+
+  it('should create a new action when add button is clicked', () => {
+    component.acts = [];
+    fixture.detectChanges();
+
+    const addButton = fixture.debugElement.query(By.css('.addItem'));
+    addButton.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    expect(component.acts.length).toBe(1);
+  });
+
+  it('should render one action editor for each action', () => {
+    component.acts = [new ViewAction(), new ViewAction()];
+    fixture.detectChanges();
+
+    const editors = fixture.debugElement.queryAll(By.directive(ActionEditorStubComponent));
+    expect(editors.length).toBe(2);
+  });
+
+  it('should pass action index and inputs to each action editor', () => {
+    const first = new ViewAction();
+    const second = new ViewAction();
+    component.acts = [first, second];
+    component.controllers = ['ctrl'];
+    component.groups = ['users'];
+    component.packageName = 'demo';
+    component.entity = 'model.entity';
+    fixture.detectChanges();
+
+    const editors = fixture.debugElement.queryAll(By.directive(ActionEditorStubComponent));
+    const firstEditor = editors[0].componentInstance as ActionEditorStubComponent;
+    const secondEditor = editors[1].componentInstance as ActionEditorStubComponent;
+
+    expect(firstEditor.obj).toBe(first);
+    expect(firstEditor.controllers).toEqual(['ctrl']);
+    expect(firstEditor.groups).toEqual(['users']);
+    expect(firstEditor.packageName).toBe('demo');
+    expect(firstEditor.entity).toBe('model.entity');
+    expect(firstEditor.actionIndex).toBe(0);
+    expect(secondEditor.actionIndex).toBe(1);
+  });
+
+  it('should remove action when action editor emits delete', () => {
+    const first = new ViewAction();
+    const second = new ViewAction();
+    component.acts = [first, second];
+    fixture.detectChanges();
+
+    const editors = fixture.debugElement.queryAll(By.directive(ActionEditorStubComponent));
+    const firstEditor = editors[0].componentInstance as ActionEditorStubComponent;
+    firstEditor.delete.emit();
+    fixture.detectChanges();
+
+    expect(component.acts).toEqual([second]);
   });
 });
