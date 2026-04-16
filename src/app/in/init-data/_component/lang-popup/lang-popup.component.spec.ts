540bce66a7eae8bad524ace532d86bbbea990c9b
LarsHanquet <145200507+LarsHanquet@users.noreply.github.com>
2026-04-16 16:51:49 +0200

    Fixed tests for lang-popup in init-data

--- src/app/in/init-data/_component/lang-popup/lang-popup.component.spec.ts
+++ src/app/in/init-data/_component/lang-popup/lang-popup.component.spec.ts
@@ -1,14 +1,39 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { WorkbenchService } from 'src/app/in/_services/workbench.service';
+import { FormControl } from '@angular/forms';
+import { of } from 'rxjs';
 import { LangPopupComponent } from './lang-popup.component';
+import { InitDataEntitySection } from '../../_models/init-data';
 
 describe('LangPopupComponent', () => {
   let component: LangPopupComponent;
   let fixture: ComponentFixture<LangPopupComponent>;
+  let mockWorkbenchService: jasmine.SpyObj<WorkbenchService>;
 
   beforeEach(async () => {
+    mockWorkbenchService = jasmine.createSpyObj('WorkbenchService', ['getSchema']);
+    
+    mockWorkbenchService.getSchema.and.returnValue(of({
+      fields: {
+        field1: { type: 'string', multilang: false, required: false },
+        field2: { type: 'number', multilang: false, required: false }
+      }
+    }));
+
+    const testData = new InitDataEntitySection(mockWorkbenchService, {
+      name: 'testEntity',
+      lang: 'en',
+      data: []
+    });
+
     await TestBed.configureTestingModule({
-      declarations: [ LangPopupComponent ]
+      declarations: [ LangPopupComponent ],
+      providers: [
+        { provide: MatDialogRef, useValue: jasmine.createSpyObj('MatDialogRef', ['close']) },
+        { provide: WorkbenchService, useValue: mockWorkbenchService },
+        { provide: MAT_DIALOG_DATA, useValue: testData }
+      ]
     })
     .compileComponents();
   });
